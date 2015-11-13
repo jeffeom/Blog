504485d5bf9d741f16851b184af9d1ba88c1a395
Jeff Eom <jeffeom@gmail.com>
2015-11-12 21:53:18 -0800

    Base completed. Now bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+
+  get '/about' => 'home#about', as: :about
+
+  resources :posts
+
+  root 'home#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
